93337540af0a5bf4942540aacd9f4fe003d7ac4d
sriharsha <sriharshanaladala999@gmail.com>
2023-02-12 20:37:25 +0530

    checking url routings and added templates folder

--- nomadic_way/places/views.py
+++ nomadic_way/places/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def sites(request):
-    return HttpResponse("best sites to visit")
+    return render(request,'places.html')
 
 
 def site(request, pk):
-    return HttpResponse("this site is one of its kind among top sites"+ ' ' +str(pk))
+    return render(request,'single-site.html')
